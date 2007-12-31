58ce9568673f33961d49048830737c2821edea88
Marti Martz <thalamew@hotmail.com>
2007-12-31 05:15:45 +0000

    Complete static file destruction of support for SeaMonkey as per vote count at [http://groups.google.com/group/greasemonkey-dev/browse_thread/thread/ccb2b0f42ea5ea3f/98cca5d9192c84f9 the discussion]
    Sync with Johan's revision of build.sh, minus SeaMonkey support, as well as mm1 branch compatibility.

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 GMMAX=0
 GMMIN=8
+GMBUILD=`date +"%Y%m%d"`
 GMREL=0
 
 GMNAME=greasemonkey
-GMCONTENT=chrome/chromeFiles/content
-GMLOCALE=chrome/chromeFiles/locale
+
+GMVER="$GMMAX.$GMMIN.$GMBUILD.$GMREL"
+GMXPI="$GMNAME-$GMVER.xpi"
 
 # Copy base structure to a temporary build directory and change to it
 rm -rf build
@@ -21,45 +23,37 @@ cd build
 
 # Generate locales for chrome.manifest from babelzilla directories, which
 # we assume have been placed in locale/.
-GMLOCALES=\"en-US\"
-for GMLANG in $(ls $GMLOCALE/); do
-  if [ $GMLANG != en-US ]; then
-    echo "locale  $GMNAME  "$GMLANG"  $GMLOCALE/"$GMLANG"/" >> chrome.manifest
-    GMLOCALES=$GMLOCALES,\ \"$GMLANG\"
-     if [ -d $GMLOCALE/$GMLANG ]; then
-       cp $GMLOCALE/en-US/contents.rdf $GMLOCALE/$GMLANG
-       sed -r -i "s/en-US/$GMLANG/g" $GMLOCALE/$GMLANG/contents.rdf
-     fi
+GMLOC=\"en-US\"
+for entry in $(ls chrome/chromeFiles/locale/); do
+  if [ $entry != en-US ]; then
+    echo "locale  $GMNAME  "$entry"  chrome/chromeFiles/locale/"$entry"/" >> chrome.manifest
+    GMLOC=$GMLOC,\ \"$entry\"
   fi
 done
 
-# Versioning checks
-GMBUILD=`date +"%04Y%02m%02d"`
-GMREGEXVER=[0-9]+\.[0-9]+\.[0-9]{8}\.[0-9]+
-
-checkGMVER () {
-  GMVER=`grep -Eo "$1" $2 | grep -Eo "$GMREGEXVER"`
-  if [ ! $GMVER ]; then
-    echo ERROR: $2 HAS INVALID VERSION!
-    exit 1
+replace () {
+  TMP=`mktemp -t Greasemonkey-build.sh`
+  SRC=`echo "$1" | sed 's/[\/\\\\]/\\\\&/g'`
+  DST=`echo "$2" | sed 's/[\/\\\\]/\\\\&/g'`
+  sed "s/$SRC/$DST/g" "$3" > "$TMP"
+  if cmp -s "$3" "$TMP" ; then
+    # No change! Treat as a failure to react to in caller.
+    rm "$TMP"
+    return 1
   fi
+  cp "$TMP" "$3"
+  rm "$TMP"
+  return 0
 }
 
-sed -r -i "s/<em:version>.*<\/em:version>/<em:version>$GMMAX\.$GMMIN\.$GMBUILD\.$GMREL<\/em:version>/" install.rdf
-checkGMVER "<em:version>$GMREGEXVER<\/em:version>" install.rdf
-
-sed -r -i "s/const APP_VERSION =.*;/const APP_VERSION = \"$GMVER\";/" install.js
-checkGMVER "const APP_VERSION = \"$GMREGEXVER\";" install.js
-
-sed -r -i "s/GM_prefRoot.setValue\(\"version\", \".*\"\);/GM_prefRoot.setValue\(\"version\", \"$GMVER\"\);/" $GMCONTENT/versioning.js
-checkGMVER "GM_prefRoot.setValue\(\"version\", \"$GMREGEXVER\"\);" $GMCONTENT/versioning.js
-
-sed -r -i "s/const APP_LOCALES =.*;/const APP_LOCALES = [ $GMLOCALES ];/" install.js
+replace '<em:version>.*</em:version>' \
+        '<em:version>'$GMVER'</em:version>' \
+        install.rdf
 
 find . -name '.svn' -prune -or -name '.DS_Store' -or -name '*~' -or -name '#*' \
-  -or -print | zip $GMNAME-$GMVER.xpi -9X -@
+  -or -print | zip -9X -@ "$GMXPI"
 
-mv $GMNAME-$GMVER.xpi ../../downloads/
+mv "$GMXPI" ../../downloads/
 
-echo Created $GMNAME-$GMVER.xpi
+echo "Created $GMXPI"
 exit 0
