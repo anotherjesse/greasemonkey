4d81456bb47b3de34d8a7f7e5507139fa8a97506
Johan Sundström <oyasumi@gmail.com>
2008-01-19 20:29:43 +0000

    Refactoring: moving code duplication into a common utils.js::GM_scriptMatchesUrl(script, url).

--- components/greasemonkey.js
+++ components/greasemonkey.js
@@ -188,30 +188,12 @@ var greasemonkeyService = {
 
   initScripts: function(url) {
     var config = new Config();
-    var scripts = [];
+    var scripts = [], script;
     config.load();
 
-    outer:
-    for (var i = 0; i < config.scripts.length; i++) {
-      var script = config.scripts[i];
-      if (script.enabled) {
-        for (var j = 0; j < script.includes.length; j++) {
-          var pattern = convert2RegExp(script.includes[j]);
-
-          if (pattern.test(url)) {
-            for (var k = 0; k < script.excludes.length; k++) {
-              pattern = convert2RegExp(script.excludes[k]);
-
-              if (pattern.test(url)) {
-                continue outer;
-              }
-            }
-
-            scripts.push(script);
-
-            continue outer;
-          }
-        }
+    for (var i = 0; script = config.scripts[i]; i++) { 
+      if (script.enabled && GM_scriptMatchesUrl(script, url)) {
+        scripts.push(script);
       }
     }
 
